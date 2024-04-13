79f8f39a0fb740aa71a3d2fef0a838c02f34f077
aksxy007 <atul13062001@gmail.com>
2024-04-13 14:37:00 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*.html"],
+  theme: {
+    extend: { 
+      colors:{
+        chatblack:{50:'#212121'},
+        sidebar:{50:'#171717'}
+    }
+  },
+   
+  },
+  plugins: [],
+}
+
