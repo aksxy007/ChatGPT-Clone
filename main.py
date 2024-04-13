a982789c374eb1f93dba434e20ba8d35b2c94c11
aksxy007 <atul13062001@gmail.com>
2024-04-13 18:10:09 +0530

    Day-2 of making chatgpt clone

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify,request
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/api", methods=["GET","POST"])
+def qa():
+    if request.method=="POST":
+        data={"result":"Thank you for the kind words! I'm glad you find ChatGPT awesome. If you have any questions or need assistance with anything, feel free to ask!"}
+        return jsonify(data)
+    data={"result":"Thank you for the kind words! I'm glad you find ChatGPT awesome. If you have any questions or need assistance with anything, feel free to ask!"}
+    return jsonify(data)
 
 
 app.run(debug = True)
